1ab91af065821a368c093f8b2c28da34e7fde37b
BpbjsGreen <3422985924@qq.com>
2025-11-27 11:37:07 +0800

    First commit.

---
+++ oi-template.cpp
@@ -0,0 +1,26 @@
+// ID: @BpbjsGreen
+
+#include <bits/stdc++.h>
+// #define int long long
+using namespace std;
+
+const int N = 2e5 + 5;
+
+void solve()
+{
+}
+
+signed main()
+{
+	// freopen(".in", "r", stdin);
+	// freopen(".out", "w", stdout);
+
+	int T;
+	scanf("%d", &T);
+	while (T--)
+	{
+		solve();
+	}
+
+	return 0;
+}
